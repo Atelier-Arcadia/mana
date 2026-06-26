dc59dc4253a52104c9a1d6e491bc7fdfd1b2f67e
Arcadia Rose <chaotic.smol@proton.me>
2026-06-26 12:01:56 -0400

    Switch from manually parsing text responses to using the tool-call API

--- src/mana/agent.clj
+++ src/mana/agent.clj
@@ -10,64 +10,36 @@
   (some #(when (= (:name %) name) %) tool-registry))
 
 (def reminder
-  "Reminder: You must always respond with JSON to declare a tool call. Plain messages will not be delivered to the user.")
+  "Reminder: You must always respond with a tool call.")
+
+(defn- format-tool-call [name args]
+  (let [fmt-name (str "(" name ")")
+        joined-args (str/join "; " args)
+        bound (min 49 (count joined-args))
+        first-50 (subs joined-args 0 bound)]
+    (str/join " " ["[Tool call]" fmt-name first-50])))
 
 (defn- handle-tool-call
-  [tool-registry assistant-message]
+  ; TODO - Update when we get schemas?
+  [tool-registry {tool-name :name tool-args :arguments}]
   "Invoke a tool as specified in a JSON-encoded tool call from the agent."
   (try
-    (let [tool-call-data (json/parse-string assistant-message)
-          tool-name (get tool-call-data "tool_name")
-          tool-args (get tool-call-data "arguments")
-          tool (find-tool tool-registry tool-name)]
-      (when tool
-        ((:implementation tool) tool-args)))
+    (let [tool (find-tool tool-registry tool-name)]
+      (if tool
+        ((:implementation tool) tool-args)
+        (str "Not a valid tool call: `" tool-name "`")))
     (catch JsonParseException e
       (str "Your response was not valid JSON\n. Error: " (.getMessage e) "\n\n" reminder))
     (catch Exception e
       (str "Tool call failed with error: " (.getMessage e)))))
 
-(defn- format-tool-description
-  [{name :name desc :description impl :implementation}]
-  (str "-`" name "` - " desc))
-
-(defn- format-tool-list [tools]
-  (str/join "\n" (map format-tool-description tools)))
-
-(defn- system-prompt [tool-registry]
-  (str "You are an orchestrator of tool calls that utilizes the tools available to you to solve the tasks the user assigns you.
+(def system-prompt
+  "You are an orchestrator of tool calls that utilizes the tools available to you to solve the tasks the user assigns you.
 
 You run within an agent harness that will respond back to you with tool call results automatically.
 Therefore, you must continue calling tools until the task is complete.
 
-The complete list of tools available to you are:\n"
-
-  (format-tool-list tool-registry)
-
-"You ALWAYS respond with a JSON-formatted response WITHOUT any markdown code block guards.
-The JSON you return MUST contain just two keys:
-- `tool_name` - A string identifying the tool to call.
-- `arguments` - A list of arguments to pass to the tool call.
-
-Do not enclose the JSON you respond with inside of Markdown code block guards.
-
-Bad:
-```json
-{ \"tool_name\": \"display\", \"arguments\": [\"Hello!\"]}
-``
-
-Bad:
-Hello!
-
-Good:
-{ \"tool_name\": \"display\", \"arguments\": [\"Hello!\"]}
-
-Guidelines:
-- If you do not call the 'display' tool, the user will not see your message.
-- All of the work you do must be through tool calls. No exceptions.
-- When your task is complete, call the 'request-input' tool to return control to the user.
-- If you do not return valid JSON, nothing will happen."))
-
+When your task is complete, call the 'request-input' tool to return control to the user to provide additional instructions.")
 
 (defn- with-retry [max-attempts f]
   (when (not (zero? max-attempts))
@@ -79,18 +51,17 @@ Guidelines:
         (with-retry (dec max-attempts) f)))))
 
 (defn agent-loop [cfg tools initial-prompt]
-  (loop [history [(inference/system-message (system-prompt tools))
+  (loop [history [(inference/system-message system-prompt)
                   (inference/user-message initial-prompt)]
          input-tokens 0
          output-tokens 0]
     (do (println "Token spend - in:" input-tokens "out:" output-tokens)
-        (let [data (with-retry 3 #(inference/inference cfg history))
-              response (:text data)
-              ;_ (println (str "Agent response:" response))
-              tool-call-result (handle-tool-call tools response)]
-              ;_ (println (str "Tool call result:" tool-call-result))]
-          (recur (conj history
-                       (inference/assistant-message response)
-                       (inference/tool-message (json/generate-string tool-call-result)))
+        ; TODO - Handle case where we get text content because lms server couldn't parse a valid tool call.
+        (let [data (with-retry 3 #(inference/inference cfg tools history))
+              tool-calls (:tool-calls data)
+              tool-call-results (map (partial handle-tool-call tools) tool-calls)
+              new-messages (interleave (map inference/tool-call-message tool-calls)
+                                       (map inference/tool-result-message tool-call-results))]
+          (recur (into history new-messages)
                  (+ input-tokens (get data :input-tokens))
                  (+ output-tokens (get data :output-tokens)))))))
--- src/mana/inference.clj
+++ src/mana/inference.clj
@@ -10,37 +10,69 @@
 (defn- text-content [text]
   { :type "text" :text text })
 
+; TODO - Not using anymore; dead code?
 (defn- extract-text-response [json-data]
   (str/join
    ""
    (map (fn [choice] (get-in choice ["message" "content"]))
         (get-in json-data ["choices"]))))
 
+(defn- extract-single-tool-call [tool-call-json]
+  (let [name (get-in tool-call-json ["function" "name"])
+        args-json (get-in tool-call-json ["function" "arguments"])
+        arguments (get (json/parse-string args-json) "arguments")]
+    {:name name
+     :arguments arguments}))
+
+(defn- extract-tool-calls [json-data]
+  (let [choices (get json-data "choices")
+        tool-call-data (flatten (map #(get-in % ["message" "tool_calls"]) choices))]
+    (map extract-single-tool-call tool-call-data)))
+
 (defn- extract-usage-data [json-data]
   (let [usage (get json-data "usage")]
     {:input-tokens (get usage "prompt_tokens")
      :output-tokens (get usage "completion_tokens")}))
 
-(def user-message #(message "user" [(text-content %)]))
-(def tool-message #(message "tool" [(text-content %)]))
-(def assistant-message #(message "assistant" [(text-content %)]))
-(def system-message #(message "system" [(text-content %)]))
-(def developer-message #(message "developer" [(text-content %)]))
+(def user-message #(message "user" %))
+(def assistant-message #(message "assistant" %))
+(def system-message #(message "system" %))
+(def tool-result-message #(message "tool" %))
+
+; TODO - Refactor code so we pass messages around rather than JSON data we have to parse through.
+(defn tool-call-message [tool-call]
+  {:role "assistant"
+   :function_call {:name (:name tool-call)
+                   :arguments {:arguments (:arguments tool-call)}}}) ; TODO - schemas :3
+
+; TODO - Provide real schemas for tools.
+(defn- tool-schemas [tools]
+  (map (fn [tool]
+         {:type "function"
+          :function {:name (:name tool)
+                     :description (:description tool)
+                     :parameters {:type "object"
+                                  :description "generic arguments interpreted by the tool"
+                                  :properties {"arguments" {:type "array" :items "string"}}}}})
+       tools))
 
 (defn inference
-  [{url :url key :api-key model :model} messages]
+  [{url :url model :model} tools messages]
   "Perform inference/completion with MiniMax-M3. Creates a hash with
 * :text
 * :input-tokens
 * :output-tokens"
-  (let [body (json/generate-string {:model model :messages messages})
+  (let [body (json/generate-string
+              {:model model
+               :messages messages
+               :tools (tool-schemas tools)}
+              {:pretty true})
         req {:accept :json
              :content-type :json
              :socket-timeout 300000
              :connection-timeout 300000
-             :headers {"Authorization" (str "Bearer " key)}
              :body body}
         res (http/post url req)
         data (json/parse-string (get res :body))]
-    (merge {:text (extract-text-response data)}
-           (extract-usage-data data))))
+    (assoc (extract-usage-data data)
+           :tool-calls (extract-tool-calls data))))
